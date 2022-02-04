55698d4cf2633371d4209d14bba7cc37c7eb6b65
Sam Serrien <sam@genx.be>
2022-02-04 18:35:39 +0100

    Cleanup with short functions

--- index.php
+++ index.php
@@ -6,51 +6,15 @@ Kirby::plugin('genxbe/ray', [
     'options' => [
 		'enabled' => false,
 	],
-	'pageMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'pagesMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'fieldMethods' => [
-		'ray' => function ($field, $color = null) {
-            return X\Ray::ray($field, $color);
-		},
-	],
-    'siteMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'fileMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'filesMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'userMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'usersMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
-    'collectionMethods' => [
-		'ray' => function ($color = null) {
-            return X\Ray::ray($this, $color);
-		},
-	],
+	'pageMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'pagesMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'fieldMethods' => ['ray' => fn ($field, $color = null) => X\Ray::ray($field, $color)],
+    'siteMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'fileMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'filesMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'userMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'usersMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
+    'collectionMethods' => ['ray' => fn ($color = null) => X\Ray::ray($this, $color)],
     'hooks' => [
         'system.exception' => function ($exception) {
             ray()->exception($exception);
